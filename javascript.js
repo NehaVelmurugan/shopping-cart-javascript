830a0dabceb9f4cd47e8c0483196181bf4d881b2
keerthijn1 <93979594+keerthijn1@users.noreply.github.com>
2021-12-11 19:00:58 +0530

    Added plusminus btn

--- javascript.js
+++ javascript.js
@@ -1,105 +1,162 @@
-let vegetables=[
-  /* Defining Arrays named vegetables*/
+﻿let vegetables = [
+    /* Defining Arrays named vegetables*/
     {
-      "category":"Organic",
-      "image":"Beans",
-      "title":"Beans" ,
-      "old-price":"80",
-      "new-price":"60",
-      "quantity":"500grms"
+        "category": "Organic",
+        "image": "Beans",
+        "title": "Beans",
+        "old-price": "80",
+        "new-price": "60",
+        "quantity": "500grms"
     },
     {
-        "category":"Organic",
-      "image":"Brinjal",
-      "title":"Brinjal" ,
-      "old-price":"40",
-      "new-price":"50",
-      "quantity":"100grms"
+        "category": "Organic",
+        "image": "Brinjal",
+        "title": "Brinjal",
+        "old-price": "40",
+        "new-price": "50",
+        "quantity": "100grms"
     },
     {
-        "category":"Organic",
-      "image":"Broclii",
-      "title":"Broclii" ,
-      "old-price":"70",
-      "new-price":"60",
-      "quantity":"300grms"
+        "category": "Organic",
+        "image": "Broclii",
+        "title": "Broclii",
+        "old-price": "70",
+        "new-price": "60",
+        "quantity": "300grms"
     },
     {
-        "category":"Organic",
-      "image":"Carrot",
-      "title":"Carrot" ,
-      "old-price":"90",
-      "new-price":"70",
-      "quantity":"400grms"
+        "category": "Organic",
+        "image": "Carrot",
+        "title": "Carrot",
+        "old-price": "90",
+        "new-price": "70",
+        "quantity": "400grms"
     },
     {
-      "category":"Organic",
-    "image":"Ladyf",
-    "title":"Ladyf" ,
-    "old-price":"90",
-    "new-price":"30",
-    "quantity":"400grms"
-  },
-  {
-    "category":"Organic",
-  "image":"Raddish",
-  "title":"Raddish" ,
-  "old-price":"50",
-  "new-price":"70",
-  "quantity":"400grms"
-},
-{
-  "category":"Organic",
-"image":"Spinach",
-"title":"Spinach" ,
-"old-price":"90",
-"new-price":"70",
-"quantity":"400grms"
-},
-{
-  "category":"Organic",
-"image":"Tomato",
-"title":"Tomato" ,
-"old-price":"90",
-"new-price":"70",
-"quantity":"200grms"
-},
+        "category": "Organic",
+        "image": "Ladyf",
+        "title": "Ladyf",
+        "old-price": "90",
+        "new-price": "30",
+        "quantity": "400grms"
+    },
+    {
+        "category": "Organic",
+        "image": "Raddish",
+        "title": "Raddish",
+        "old-price": "50",
+        "new-price": "70",
+        "quantity": "400grms"
+    },
+    {
+        "category": "Organic",
+        "image": "Spinach",
+        "title": "Spinach",
+        "old-price": "90",
+        "new-price": "70",
+        "quantity": "400grms"
+    },
+    {
+        "category": "Organic",
+        "image": "Tomato",
+        "title": "Tomato",
+        "old-price": "90",
+        "new-price": "70",
+        "quantity": "200grms"
+    },
 
-  ]
+]
+
+let itemCount = 0;
   
- function loded()/*defining the function loaded*/
-    {
-    
-      vegetables.forEach((obj,index)=>{
-        console.log(obj);
-       
-         
-        let html= `
-        <div id="${index}" class="p-2 col-xl-3 col-lg-4 col-md-4 col-sm-6 col-xs-12">
-        <div class="card">
-                  <div class="box-content p-2">
-                  <div class="top-left">Organic</div>
-            <center>  <img class="img" src="./images1/${obj.image}.jpg" height="80px" width="110px"></center> /*displays image*/
-                 <p style="display:"inline",float:"left"">${obj.title}</p>
-                 <table class="table table-borderless ">
-                 <tr>
-                 <td style="text-align:"right;"">  <p style="text-align:end;"><span class="strike" style="text-align:"right;"">₹${obj["old-price"]}</span></p></td>
-                 <td> <p  ><strong>₹${obj["new-price"]}</strong></p></td>
-                 </tr>
-                 </table>
+function loded()
+   {
+   
+     vegetables.forEach((obj,index)=>{
+       console.log(obj);
+ 
+        
+       let html= `
+       <div id="${index}" class="p-2 col-xl-3 col-lg-3 col-md-4 col-sm-6 col-xs-12" >
+       <div class="card">
+                 <div class="box-content" >
+                 <div class="top-left">Organic</div>
+           <center>  <img class="img" src="./images1/${obj.image}.jpg" height="80px" width="130px"></center>
+                <p style="display:"inline",float:"left"">${obj.title}</p>
+                <table class="table table-borderless ">
+                <tr>
+                <td style="text-align:"right;"">  <p style="text-align:end;"><span class="strike" style="text-align:"right;"">₹${obj["old-price"]}</span></p></td>
+                <td> <p  ><strong>₹${obj["new-price"]}</strong></p></td>
+                </tr>
+                </table>
+              
+                
+                <p  style="display:"inline",text-align:"left";">${obj.quantity}</p>
+                                      
+                           <button class="Btn_Cart" id="Btn-${index}">Add To Cart</button>
+                           <div id="post_addToCardDiv" style="display:none;">
+                               <div id="-${index}" class="plus_minus_btn">-</div>
+                               <div id="{itemCount-${index}}">0</div>
+                               <div id="+${index}" class="plus_minus_btn">+</div>
+                           </div>
+                                      
+                </div>
+                </div>
+             </div>`
+         /*added plus minus btn*/
+          document.querySelector('.container').insertAdjacentHTML('afterbegin', html);
+
+  
+     })
+   
+    document.querySelectorAll(".plus_minus_btn").forEach((item) => {
+        item.addEventListener('click', event => {
+            var targetedButton = event.target.id;
+            
+            
+            if (targetedButton.charAt(0) == "+") {
+                var itemC = document.getElementById(targetedButton).previousElementSibling;
+                itemCount = itemC.innerHTML;
+                itemCount++;
+                itemC.innerHTML = itemCount;
+            }
+            else {
+                
+                var itemC = document.getElementById(targetedButton).nextElementSibling;
+                itemCount = itemC.innerHTML;
+                itemCount = itemCount == 0 ? 0 : itemCount - 1;
+                if (itemCount == 0) {
+                    document.getElementById(targetedButton).parentElement.style.display = "none";
+                    document.getElementById(targetedButton).parentElement.previousElementSibling.style.display = "block";
+                }
+
+                itemC.innerHTML = itemCount;
                
-                 
-                 <p  style="display:"inline",text-align:"left";">${obj.quantity}</p>
-                 <button class="Btn_Cart" onclick=Cliq()>Add To Cart</button>
-                 </div>
-                 </div>
-              </div>`
-           document.querySelector('.container').insertAdjacentHTML('afterbegin', html);/* retrieve the selector class code called as .container*/
+            }
+            
+
+        })
+    })
 
    
-      })
-    }
-   
+    document.querySelectorAll(".Btn_Cart").forEach((item) => {
+        item.addEventListener('click', event => {
+            var targetedButton = event.target.id;
+            var sibling = document.getElementById(targetedButton).nextElementSibling;
+            sibling.style.display = 'flex';
+            
+            document.getElementById(targetedButton).style.display = 'none';
+
+            var itemC = document.getElementById(targetedButton).nextElementSibling.children[1];
+            itemCount = itemC.innerHTML;
+            itemCount++;
+            itemC.innerHTML = itemCount;
+        })
+    })
+    
+             };
+
+
     // <div class="row">
     //     <div class="column">
     //       <div class="card">
@@ -110,6 +167,6 @@ let vegetables=[
     //         <p><span class="strike">₹40</span>₹23</p>
     //         <p class="title_item">Approx 500grams</p>
     //         <button class="Btn_Cart" onclick="Cliq()">Add To Cart</button><!--cart button-->
-            
+
     //       </div>
     //       </div>
