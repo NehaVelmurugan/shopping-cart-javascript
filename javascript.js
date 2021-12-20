83221b263331ca0bf584dfe5919331ca7a6b60cc
keerthijn1 <93979594+keerthijn1@users.noreply.github.com>
2021-12-20 22:04:30 +0530

    using json and  quantity number

--- javascript.js
+++ javascript.js
@@ -1,271 +1,50 @@
-﻿let Gourment = {
-    vegetables :[
-        /* Defining Arrays named vegetables*/
-        {
-            "category": "Organic",
-            "image": "Beans",
-            "title": "Beans",
-             "displayName":"beans",
-            "old-price": "80",
-            "new-price": "60",
-            "quantity": "500grms",
-            "count":0
-        },
-        {
-            "category": "Organic",
-            "image": "Brinjal",
-            "title": "Brinjal",
-             "displayName":"brinjal",
-            "old-price": "40",
-            "new-price": "50",
-            "quantity": "100grms",
-            "count":0
-        },
-        {
-            "category": "Organic",
-            "image": "Broclii",
-            "title": "Broclii",
-             "displayName":"broclii",
-            "old-price": "70",
-            "new-price": "60",
-            "quantity": "300grms",
-            "count":0
-        },
-        {
-            "category": "Organic",
-            "image": "Carrot",
-            "title": "Carrot",
-             "displayName":"Carrot",
-            "old-price": "90",
-            "new-price": "70",
-            "quantity": "400grms",
-            "count":0
-        },
-        {
-            "category": "Organic",
-            "image": "Ladyf",
-            "title": "Ladyf",
-            "displayName":"ladyf",
-            "old-price": "90",
-            "new-price": "30",
-            "quantity": "400grms",
-            "count":0
-
-        },
-        {
-            "category": "Organic",
-            "image": "Raddish",
-            "title": "Raddish",
-            "displayName":"raddish",
-            "old-price": "50",
-            "new-price": "70",
-            "quantity": "400grms",
-            "count":0
-        },
-        {
-            "category": "Organic",
-            "image": "Spinach",
-            "title": "Spinach",
-             "displayName":"spinach",
-            "old-price": "90",
-            "new-price": "70",
-            "quantity": "400grms",
-            "count":0
-        },
-        {
-            "category": "Organic",
-            "image": "Tomato",
-            "title": "Tomato",
-             "displayName":"tomato",
-            "old-price": "90",
-            "new-price": "70",
-            "quantity": "200grms",
-            "count":0
-        },
-    ],
-
-    leafyGreen:[
-        /* Defining Arrays named Leafy_green*/
-        {
-            "category": "Organic",
-            "image": "Baby Butterhead lettuce",
-            "title": "Baby Butterhead lettuce",
-            "displayName":"Baby Butterhead lettuce",
-            "old-price": "80",
-            "new-price": "60",
-            "quantity": "500grms",
-            "count":0
-        },
-        {
-            "category": "Organic",
-            "image": "Rocket(Arugula)",
-            "title": "Rocket(Arugula)",
-            "displayName":"Rocket(Arugula)",
-            "old-price": "30",
-            "new-price": "50",
-            "quantity": "100grms",
-            "count":0
-        },
-        {
-            "category": "Organic",
-            "image": "curry Leavs",
-            "title": "curry Leavs",
-            "displayName":"curry Leavs",
-            "old-price": "20",
-            "new-price": "40",
-            "quantity": "100grms",
-            "count":0
-        },
-        {
-            "category": "Organic",
-            "image": "mint",
-            "title": "mint",
-            "displayName":"mint",
-            "old-price": "40",
-            "new-price": "50",
-            "quantity": "100grms",
-            "count":0
-        },
-        {
-            "category": "Organic",
-            "image": "Rocket(Arugula)",
-            "title": "Rocket(Arugula)",
-            "displayName":"Rocket(Arugula)",
-            "old-price": "60",
-            "new-price": "30",
-            "quantity": "200grms",
-            "count":0
-        },
-    ],
-
-    freshlyCut:[
-        /* Defining Arrays named Freshlycut*/
-        {
-            "category": "Organic",
-            "image": "broccolifloret",
-            "title": "broccolifloret",
-             "displayName":"broccoliFloret",
-            "old-price": "100",
-            "new-price": "30",
-            "quantity": "100grms",
-            "count":0
-        },
-        {
-            "category": "Organic",
-            "image": "cabbagegreenshred",
-            "title": "cabbagegreensshred",
-            "displayName":"cabbagegreensshred",
-            "old-price": "100",
-            "new-price": "30",
-            "quantity": "100grms",
-            "count":0
-        },
-        {
-            "category": "Organic",
-            "image": "cauliflowerfloret",
-            "title": "cauliflowerfloret",
-           "displayName":"cauliflowerFloret",
-            "old-price": "20",
-            "new-price": "40",
-            "quantity": "100grms",
-            "count":0
-        },
-        {
-            "category": "Organic",
-            "image": "garlicpeel",
-            "title": "Garlicpeel",
-            "displayName":"garlicpeel",
-            "old-price": "40",
-            "new-price": "50",
-            "quantity": "200grms",
-            "count":0
-        },
-        {
-            "category": "Organic",
-            "image": "greenpeaspeeled",
-            "title": "greenpeaspeeled",
-            "displayName":"greenpeasPeeled",
-            "old-price": "60",
-            "new-price": "30",
-            "quantity": "50grms",
-            "count":0
-        },
-        {
-            "category": "Organic",
-            "image": "pineapple-peeled & sliced",
-            "title": "pineapple-peeled & sliced",
-             "displayName":"pineapplesPeeled & sliced",
-            "old-price": "70",
-            "new-price": "100",
-            "quantity": "70grms",
-            "count":0
-        },
-    ],
-    dryFruits:[
-        /* Defining Arrays named Dry_Fruits*/
-        {
-            "category": "Organic",
-            "image": "cashewsclassic",
-            "title": "cashewclassic",
-             "displayName":"cashewClassic",
-            "old-price": "300",
-            "new-price": "400",
-            "quantity": "100grms",
-            "count":0
-        },
-        {
-            "category": "Organic",
-            "image": "Classic Almond-California",
-            "title": "Classic Almond-California",
-             " displayName":"classicAlmond California",
-            "old-price": "200",
-            "new-price": "350",
-            "quantity": "100grms",
-            "count":0
-        },
-        {
-            "category": "Organic",
-            "image": "pumpkinseeds",
-            "title": "Pumpkinseed",
-            " displayName":"pumpkinseed",
-            "old-price": "100",
-            "new-price": "120",
-            "quantity": "50grms",
-            "count":0
-        },
-        {
-            "category": "Organic",
-            "image": "raisinsblack",
-            "title": "raisinsblack",
-            " displayName":"raisinsblack",
-            "old-price": "40",
-            "new-price": "50",
-            "quantity": "200grms",
-            "count":0
-        },
-        {
-            "category": "Organic",
-            "image": "walnut",
-            "title": "greenpeaspeeled",
-           " displayName":"greenpeas peeled",
-            "old-price": "60",
-            "new-price": "30",
-            "quantity": "50grms",
-            "count":0
-        },
-    ]
+﻿let Gourment = {}
+const getJsonFile = () => {
+    fetch("./items.json").then(async response => {
+      try {
+       const data = await response.json()
+          console.log('response data?', data)
+          Gourment= data
+         
+     } catch(error) {
+       console.log('Error happened here!')
+       console.error(error)
+     }
+    })
     
-};
+}
+
+ function onLoad(){
+ fetch("./items.json").then(async response => {
+      try {
+       const data = await response.json()
+          console.log('response data?', data)
+          Gourment = data
+          
+         loded('vegetables') 
+         
+     } catch(error) {
+       console.log('Error happened here!')
+       console.error(error)
+     }
+    })
+
+  
+}
+  
+
 
 let selecedItems=[]
-
 let itemCount = 0; 
 let cartCount=0;/Keep Track of Cart_Count/
-let cop_item=0;
-function loded(opt)
+let Fn_Call=0;
+ function loded(opt)
 
-{  
-        console.log(opt,Gourment[opt])
+{ 
+   
+   
+
+    console.log(opt, Gourment[opt])
      document.querySelector('.container').innerHTML="";/* Clear the Screen Before proceeding to next Screen*/
      Gourment[opt].forEach((obj,index)=>{
        console.log(obj);
@@ -298,7 +77,9 @@ function loded(opt)
            .insertAdjacentHTML('afterbegin', html);
      })
         document.querySelectorAll(".plus_minus_btn").forEach((item) => {
-        item.addEventListener('click', event => {
+            item.addEventListener('click', event => {
+         
+
             var targetedButton = event.target.id;
             if (targetedButton.charAt(0) == "+") {
                 var itemC = document.getElementById(targetedButton).previousElementSibling;
@@ -356,6 +137,7 @@ function loded(opt)
         })
     })
     document.querySelectorAll(".Btn_Cart").forEach((item) => {
+
         item.addEventListener('click', event => {
             var targetedButton = event.target.id;
             var sibling = document.getElementById(targetedButton).nextElementSibling;
@@ -364,7 +146,7 @@ function loded(opt)
             var itemC = document.getElementById(targetedButton).nextElementSibling.children[1];
             itemCount = itemC.innerHTML;
             itemCount++;
-            itemC.innerHTML = 1;
+            itemC.innerHTML = Gourment[opt][targetedButton.charAt(4)].count+1;
             document.getElementById("checkoutCart").innerHTML = ++cartCount; 
             Gourment[opt][targetedButton.charAt(4)].count=Gourment[opt][targetedButton.charAt(4)].count+1;
             if (selecedItems.some(elem => elem.title === Gourment[opt][targetedButton.charAt(4)].title)) {
