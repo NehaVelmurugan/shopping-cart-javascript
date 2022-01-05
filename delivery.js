65720bfdc028609c811bf336f038903938aead21
keerthijn1 <93979594+keerthijn1@users.noreply.github.com>
2022-01-06 01:30:23 +0530

    added thank you page

--- delivery.js
+++ delivery.js
@@ -1,74 +1,120 @@
-const form = document.getElementById('form');
-const username = document.getElementById('username');
-const email = document.getElementById('email');
-const phonno = document.getElementById('Phonno');
-const Address = document.getElementById('Address');
-
-form.addEventListener('submit', e => {
-    e.preventDefault();
-
-    validateInputs();
-});
-
-const setError = (element, message) => {
-    const inputControl = element.parentElement;
-    const errorDisplay = inputControl.querySelector('.error');
-
-    errorDisplay.innerText = message;
-    inputControl.classList.add('error');
-    inputControl.classList.remove('success')
-}
-
-const setSuccess = element => {
-    const inputControl = element.parentElement;
-    const errorDisplay = inputControl.querySelector('.error');
-
-    errorDisplay.innerText = '';
-    inputControl.classList.add('success');
-    inputControl.classList.remove('error');
-};
-
-const isValidEmail = email => {
-    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
-}
-
-const validateInputs = () => {
-    const usernameValue = username.value.trim();
-    const emailValue = email.value.trim();
-    const PhonnoValue = Phonno.value.trim();
-    const AddressValue = Address.value.trim();
-
-    if(usernameValue === '') {
-        setError(username, 'Username is required');
+class FormValidation{
+    formValues = {
+        username : "",
+        email : "",
+        phonenumber : "",
+        password : ""
     }
-    else if (usernameValue.length < 3) {
-        setError(username, 'username must be at least 3 characters.')
+    errorValues = {
+        usernameErr : "",
+        emailErr : "",
+        phonenumberErr : "",
+        passwordErr : ""
     }
-    else {
-        setSuccess(username);
+    showErrorMsg(index,msg){
+        const form_group = document.getElementsByClassName('form-group')[index]
+        form_group.classList.add('error')
+        form_group.getElementsByTagName('span')[0].textContent = msg   
+    }
+    showSuccessMsg(index){
+        const form_group = document.getElementsByClassName('form-group')[index]
+        form_group.classList.remove('error')
+        form_group.classList.add('success')
+    }
+    getInputs(){
+        this.formValues.username = document.getElementById('username').value.trim()
+        this.formValues.email = document.getElementById('email').value.trim()
+        this.formValues.phonenumber = document.getElementById('phonenumber').value.trim()
+        this.formValues.password = document.getElementById('password').value.trim()
+    }
+    validateUsername(){
+        if(this.formValues.username === ""){
+            this.errorValues.usernameErr = "* Please Enter Your Name"
+            this.showErrorMsg(0,this.errorValues.usernameErr)
+        } else if(this.formValues.username.length <= 4 ){
+            this.errorValues.usernameErr = "* Username must be atleast 5 Characters"
+            this.showErrorMsg(0,this.errorValues.usernameErr)
+        } else if(this.formValues.username.length > 14){
+            this.errorValues.usernameErr = "* Username should not exceeds 14 Characters"
+            this.showErrorMsg(0,this.errorValues.usernameErr)
+        } else {
+            this.errorValues.usernameErr = ""
+            this.showSuccessMsg(0)
+        }
+    }
+    validateEmail(){
+        //abc@gmail.co.in
+        const regExp = /^([a-zA-Z0-9-_\.]+)@([a-zA-Z0-9]+)\.([a-zA-Z]{2,10})(\.[a-zA-Z]{2,8})?$/
+        if(this.formValues.email === ""){
+            this.errorValues.emailErr = "* Please Enter Valid Email"
+            this.showErrorMsg(1,this.errorValues.emailErr)
+        } else if(!(regExp.test(this.formValues.email))){
+            this.errorValues.emailErr = "* Invalid Email"
+            this.showErrorMsg(1,this.errorValues.emailErr)
+        } else {
+            this.errorValues.emailErr = ""
+            this.showSuccessMsg(1)
+        }
+    }
+    validatePhonenumber(){
+       const phoneno = /^\d{10}$/
+       if(this.formValues.phonenumber === ""){
+           this.errorValues.phonenumberErr = "* Please Enter your Phone Number"
+           this.showErrorMsg(2,this.errorValues.phonenumberErr)
+       } else if(phoneno.test(this.formValues.phonenumber)){
+           this.errorValues.phonenumberErr = ""
+           this.showSuccessMsg(2)
+       } else {
+           this.errorValues.phonenumberErr = "* Invalid Phone Number"
+           this.showErrorMsg(2,this.errorValues.phonenumberErr)
+       }
+    }
+    validatePassword(){
+        if(this.formValues.password === ""){
+            this.errorValues.passwordErr = "* Please Provide a Password"
+            this.showErrorMsg(3,this.errorValues.passwordErr)
+        } else if(this.formValues.password.length <= 4){
+            this.errorValues.passwordErr = "* Password must be atleast 5 Characters"
+            this.showErrorMsg(3,this.errorValues.passwordErr)
+        } else if(this.formValues.password.length > 10){
+            this.errorValues.passwordErr = "* Password should not exceeds 10 Characters"
+            this.showErrorMsg(3,this.errorValues.passwordErr)
+        } else {
+            this.errorValues.passwordErr = ""
+            this.showSuccessMsg(3)
+        }
+    }
+    alertMessage(){
+        const {usernameErr , emailErr , phonenumberErr , passwordErr , confirmpasswordErr}= this.errorValues
+        if(usernameErr === "" && emailErr === "" && phonenumberErr === "" && passwordErr === ""){
+            swal("UserDetails Successful","ThankYou , "+this.formValues.username,"success").then(() => {
+                console.log(this.formValues)
+                this.removeInputs()
+            })
+        } else {
+            swal("Give Valid Inputs","Click ok to Continue" ,"error")
+        }
     }
 
-    if(emailValue === '') {
-        setError(email, 'Email is required');
-    } else if (!isValidEmail(emailValue)) {
-        setError(email, 'Provide a valid email address');
-    } else {
-        setSuccess(email);
+    removeInputs(){
+        const form_groups = document.getElementsByClassName('form-group')
+        Array.from(form_groups).forEach(element => {
+            element.getElementsByTagName('input')[0].value = ""
+            element.getElementsByTagName('span')[0].textContent = ""
+            element.classList.remove('success')
+        })
     }
+} 
 
-    if(PhonnoValue === '') {
-        setError(Phonno, 'Phonno is required');
-    } else if (PhonnoValue.length < 8 ) {
-        setError(Phonno, 'Phonno must be at least 10 numbers.')
-    } else {
-        setSuccess(Phonno);
-    }
+const ValidateUserInputs = new FormValidation()
 
-    if(AddressValue === '') {
-        setError(Address, 'Provide valid address');
-    } else {
-        setSuccess(Address);
-    }
+document.getElementsByClassName('form')[0].addEventListener('submit' , event => {
+    event.preventDefault()
+    ValidateUserInputs.getInputs()
+    ValidateUserInputs.validateUsername()
+    ValidateUserInputs.validateEmail()
+    ValidateUserInputs.validatePhonenumber()
+    ValidateUserInputs.validatePassword()
+    ValidateUserInputs.alertMessage()
+})
 
-};
