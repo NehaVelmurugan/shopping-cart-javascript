6313489b03f2280e261e873ae47a433125a68717
keerthijn1 <93979594+keerthijn1@users.noreply.github.com>
2022-01-11 13:39:13 +0530

    created checkoutform

--- delivery.js
+++ delivery.js
@@ -1,12 +1,12 @@
 class FormValidation{
     formValues = {
-        username : "",
+        name : "",
         email : "",
         phonenumber : "",
         Address: ""
     }
     errorValues = {
-        usernameErr : "",
+        nameErr : "",
         emailErr : "",
         phonenumberErr : "",
         AddressErr : ""
@@ -22,33 +22,36 @@ class FormValidation{
         form_group.classList.add('success')
     }
     getInputs(){
-        this.formValues.username = document.getElementById('username').value.trim()
+        this.formValues.name = document.getElementById('name').value
         this.formValues.email = document.getElementById('email').value.trim()
         this.formValues.phonenumber = document.getElementById('phonenumber').value.trim()
-        this.formValues.Address = document.getElementById('Address').value.trim()
+        this.formValues.Address = document.getElementById('Address').value
     }
-    validateUsername(){
-        if(this.formValues.username === ""){
-            this.errorValues.usernameErr = "* Please Enter Your Name"
-            this.showErrorMsg(0,this.errorValues.usernameErr)
-        } else if(this.formValues.username.length <= 2 ){
-            this.errorValues.usernameErr = "* Username must be atleast 2 Characters"
-            this.showErrorMsg(0,this.errorValues.usernameErr)
-        } else if(this.formValues.username.length > 14){
-            this.errorValues.usernameErr = "* Username should not exceeds 14 Characters"
-            this.showErrorMsg(0,this.errorValues.usernameErr)
+    validatename(){
+        if(this.formValues.name === ""){
+            this.errorValues.nameErr = "* Please Enter Your Name"
+            this.showErrorMsg(0,this.errorValues.nameErr)
+        } else if(this.formValues.name.length <= 2 ){
+            this.errorValues.nameErr = "* name must be atleast 2 Characters"
+            this.showErrorMsg(0,this.errorValues.nameErr)
+        } else if(this.formValues.name.length > 14){
+            this.errorValues.nameErr = "* name should not exceeds 14 Characters"
+            this.showErrorMsg(0,this.errorValues.nameErr)
         } else {
-            this.errorValues.usernameErr = ""
+            this.errorValues.nameErr = ""
             this.showSuccessMsg(0)
         }
     }
+    
     validateEmail(){
         //abc@gmail.co.in
         const regExp = /^([a-zA-Z0-9-_\.]+)@([a-zA-Z0-9]+)\.([a-zA-Z]{2,10})(\.[a-zA-Z]{2,8})?$/
+        //compares datatype and value
         if(this.formValues.email === ""){
             this.errorValues.emailErr = "* Please Enter Valid Email"
             this.showErrorMsg(1,this.errorValues.emailErr)
-        } else if(!(regExp.test(this.formValues.email))){
+        }//test is used for matching a pattern and value
+        else if (!(regExp.test(this.formValues.email))) {
             this.errorValues.emailErr = "* Invalid Email"
             this.showErrorMsg(1,this.errorValues.emailErr)
         } else {
@@ -74,7 +77,7 @@ class FormValidation{
             this.errorValues.AddressErr = "* Please Provide a Addresss"
             this.showErrorMsg(3,this.errorValues.AddressErr)
         } else if(this.formValues.Address.length <= 2){
-            this.errorValues.AddressErr = "* address  1 characters  mandatory"
+            this.errorValues.AddressErr = "* address  2 characters  mandatory"
             this.showErrorMsg(3,this.errorValues.AddressErr)
         } else if(this.formValues.Address.length > 50){
             this.errorValues.AddressErr = "* Address should not exceeds 50 Characters"
@@ -85,9 +88,9 @@ class FormValidation{
         }
     }
     alertMessage(){
-        const {usernameErr , emailErr , phonenumberErr , AddressErr}= this.errorValues
-        if(usernameErr === "" && emailErr === "" && phonenumberErr === "" && AddressErr === ""){
-            swal("User Details Successful","ThankYou , "+this.formValues.username,"success").then(() => {
+        const {nameErr , emailErr , phonenumberErr , AddressErr}= this.errorValues
+        if(nameErr === "" && emailErr === "" && phonenumberErr === "" && AddressErr === ""){
+            swal("User Details Successful","ThankYou , "+this.formValues.name,"success").then(() => {
                 console.log(this.formValues)
                 this.removeInputs()
             })
@@ -111,10 +114,87 @@ const ValidateUserInputs = new FormValidation()
 document.getElementsByClassName('form')[0].addEventListener('submit' , event => {
     event.preventDefault()
     ValidateUserInputs.getInputs()
-    ValidateUserInputs.validateUsername()
+    ValidateUserInputs.validatename()
     ValidateUserInputs.validateEmail()
     ValidateUserInputs.validatePhonenumber()
     ValidateUserInputs.validateAddress()
     ValidateUserInputs.alertMessage()
 })
 
+function Activate() {
+    let Name = document.getElementById("name").value;
+    let emailId = document.getElementById("email").value;
+    let Phonno = document.getElementById("phonenumber").value;
+    let Address = document.getElementById("Address").value;
+    if (Name != "" && emailId != "" && Phonno.length !="" && Address != "") {
+        document.getElementById("submit").removeAttribute("disabled");
+        document.getElementById("submit").style.backgroundColor=" #6e8e4b";
+    }
+    else {
+         document.getElementById("submit").style.backgroundColor="gray";
+    }
+    
+}
+
+
+document.getElementById("name").addEventListener('keyup', (event) => {
+      event.preventDefault()
+    ValidateUserInputs.getInputs()
+    ValidateUserInputs.validatename()
+
+})
+
+document.getElementById("email").addEventListener('keyup', (event) => {
+      event.preventDefault()
+    ValidateUserInputs.getInputs()
+    ValidateUserInputs.validateEmail()
+
+})
+
+document.getElementById("phonenumber").addEventListener('keyup', (event) => {
+      event.preventDefault()
+    ValidateUserInputs.getInputs()
+    ValidateUserInputs.validatePhonenumber()
+
+})
+
+document.getElementById("Address").addEventListener('keyup', (event) => {
+      event.preventDefault()
+    ValidateUserInputs.getInputs()
+    ValidateUserInputs.validateAddress()
+
+})
+
+document.getElementById("email").addEventListener('click', (event) => {
+      event.preventDefault()
+    ValidateUserInputs.getInputs()
+    ValidateUserInputs.validateEmail()
+     ValidateUserInputs.validatename()
+
+})
+
+document.getElementById("phonenumber").addEventListener('click', (event) => {
+      event.preventDefault()
+    ValidateUserInputs.getInputs()
+    ValidateUserInputs.validatePhonenumber()
+    ValidateUserInputs.validatename()
+    ValidateUserInputs.validateEmail()
+
+})
+
+document.getElementById("Address").addEventListener('click', (event) => {
+      event.preventDefault()
+    ValidateUserInputs.getInputs()
+    ValidateUserInputs.validateAddress()
+     ValidateUserInputs.validatePhonenumber()
+    ValidateUserInputs.validatename()
+    ValidateUserInputs.validateEmail()
+
+})
+
+// function thankyou() {
+//     window.location.href = "thank.html";
+// }
+function Previous() {
+    window.location.href = "delivery.html";
+}
